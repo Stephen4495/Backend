5a9acb49ed494c6ec25ff7e8d193b7de9579f8bf
Stephen Muritu <muritustephen1@gmail.com>
2024-12-27 00:14:22 +0300

    Setting up the server to serve HTML and static files

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -1,8 +1,24 @@
-import express from "express"
+import express from 'express'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+//Get the file path form URL of the current module
+const __filename = fileURLToPath(import.meta.url);
+//Get the directory name form the file path
+const __dirname = dirname(__filename);
+
+//Serves the HTML file from the /public direction and 
+//Tells express to serve all files from the public folder as static asssets/files. Any request for the css files will be resolved to the public directory
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+// Serving up the HTML from the /public directory
+app.get('/', (req, res)=>{
+    res.sendFile(Path.join(__dirname, 'public', 'index.html'))
+});
 
 app.listen(PORT, ()=>{
   console.log(`Server has started on port: ${PORT}`);
