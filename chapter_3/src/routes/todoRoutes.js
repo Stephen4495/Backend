9064d9f59ebdbf7456b327be7dd68ce69c7ddd64
Stephen Muritu <muritustephen1@gmail.com>
2024-12-27 01:39:52 +0300

    Creating routes

---
+++ chapter_3/src/routes/todoRoutes.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import db from '../db.js';
+
+const router = express.Router();
+
+// Get all todos for loogged-in user
+router.get('/', (req, res)=>{
+
+});
+// Create a new todo
+router.post('/', (req, res)=>{
+
+});
+
+// Update a todo
+router.put('/:id', (req, res)=>{
+
+});
+
+// DElete a todo
+router.delete('/:id', (req, res)=>{
+
+});
+
+export default router;
--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -1,27 +1,34 @@
-import express from 'express'
-import path, { dirname } from 'path'
+import express from 'express';
+import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import authRoutes from './routes/authRoutes.js';
+import todoRoutes from './routes/todoRoutes.js'
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//Get the file path form URL of the current module
+// Get the file path from the URL of the current module
 const __filename = fileURLToPath(import.meta.url);
-//Get the directory name form the file path
+// Get the directory name from the file path
 const __dirname = dirname(__filename);
 
-//MIDDLEWARE
-app.use(express.json)
-//Serves the HTML file from the /public direction and 
-//Tells express to serve all files from the public folder as static asssets/files. Any request for the css files will be resolved to the public directory
-app.use(express.static(path.join(__dirname, '../public')))
+// MIDDLEWARE
+app.use(express.json()); 
+// Serves the HTML file from the /public directory
+// Tells Express to serve all files from the public folder as static assets/files. Any request for CSS files will resolve to the public directory
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Serving up the HTML from the /public directory
-app.get('/', (req, res)=>{
-    res.sendFile(Path.join(__dirname, 'public', 'index.html'))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html')); // Fixed path module reference
 });
 
-app.listen(PORT, ()=>{
+// Routes
+app.use('/auth', authRoutes);
+app.use('/todos',todoRoutes);
+
+
+app.listen(PORT, () => {
   console.log(`Server has started on port: ${PORT}`);
 });
 
